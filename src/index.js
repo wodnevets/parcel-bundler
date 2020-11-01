cf9a53f4c6b5c9b2e6ac80d17f2287faa98eba1d
StevanDow <thefamousunknown@gmail.com>
2020-11-01 12:17:21 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './scripts/scroll-to-top';
+import './scripts/toggle'
+import './scripts/dark-mode'
+import Scrollbar from 'smooth-scrollbar';
+
+Scrollbar.init(document.querySelector('#my-scrollbar'), options);
